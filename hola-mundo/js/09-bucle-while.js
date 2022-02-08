441ca4c4a695b59b2a6cf65677600b787c41ae69
drPabloIgnacio <gdiaz.pabloignacio@gmail.com>
2022-02-08 01:32:59 -0300

    practicando bucle while

---
+++ hola-mundo/js/09-bucle-while.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/* While es menos estricto que el for y podemos hacer bucles infinitos. pero debemos saber
+quebrarlo */
+
+var year = 2018
+
+ while (year <= 2051) {
+     // ejecuta esto
+     console.log("Estamos en el año: " + year);
+     year++;
+}
+
+/* Decrementar
+while (year != 1991) {
+    // ejecuta esto
+    console.log("Estamos en el año: " + year);
+    year--;
+}
+*/
