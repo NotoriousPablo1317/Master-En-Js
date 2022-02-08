a0b6762ccb212e25d66c3eeea888164d6f779a38
Dr. Pablo Ignacio <90404984+drPabloIgnacio@users.noreply.github.com>
2022-02-07 22:03:08 -0300

    Practicando JS
    En esta carpeta se encuentran varios archivos js con los cuales se practicaron.

---
+++ hola-mundo/js/01-hola-mundo.js
@@ -0,0 +1,7 @@
+alert("Hola mundo con JS");
+alert("Siempre es bueno repasar conceptos");
+document.write("Llamada desde archivo externo");
+
+// Visualizando en consola del navegador
+console.log("Muestra esto en la consola");
+console.log(15+15)
