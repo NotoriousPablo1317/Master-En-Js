e8684c934c1498665ae5d2d5a9b33ec1ad17ff00
drPabloIgnacio <gdiaz.pabloignacio@gmail.com>
2022-02-08 01:45:11 -0300

    practicando bucle while, do while y break

--- hola-mundo/js/09-bucle-while.js
+++ hola-mundo/js/09-bucle-while.js
@@ -1,20 +1,32 @@
 'use strict'
 
+var year = 2018
 /* While es menos estricto que el for y podemos hacer bucles infinitos. pero debemos saber
 quebrarlo */
-
-var year = 2018
-
+/*
  while (year <= 2051) {
      // ejecuta esto
      console.log("Estamos en el año: " + year);
      year++;
 }
+*/
 
-/* Decrementar
+// Decrementar
 while (year != 1991) {
     // ejecuta esto
     console.log("Estamos en el año: " + year);
+
+    if(year == 2000){
+        break;
+    }
+
     year--;
 }
-*/
+
+// Do while
+var years = 26;
+
+do{
+    alert("Solo cuando sea diferente a 20")
+    years--;
+} while(years > 25)
