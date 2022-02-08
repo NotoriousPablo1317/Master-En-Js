9107e5db7fe79dd832319b0229f46112c1be7a0f
Dr. Pablo Ignacio <90404984+drPabloIgnacio@users.noreply.github.com>
2022-02-07 22:23:35 -0300

    trabajando variables js
    trabajando variables js

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,15 @@
+// Variables
+// Es un contenedor de informacion.
+
+var pais = "Chile";
+var continente = "Latinoamerica";
+var antiguedad = 2022;
+// Concatenar 2 cadenas de texto
+var pais_y_continente = pais+' '+continente;
+
+pais = "Turquia";
+continente = "Europa"
+
+console.log(pais, continente, antiguedad)
+// llamara las primeras variables que declaramos Chile y latam
+alert(pais_y_continente)
