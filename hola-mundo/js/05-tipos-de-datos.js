843e0aa5f7230540081805f1f56616701571b1e8
drPabloIgnacio <gdiaz.pabloignacio@gmail.com>
2022-02-07 23:25:09 -0300

    trabajando con tipos de datos y operadores

--- hola-mundo/js/05-tipos-de-datos.js
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -1,5 +1,22 @@
 'use strict'
+// alert('comprobando la carga')
 
 // Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2; // puede ocupar -, *, /, % saca el resto de la division entre 2 numeros.
+
+alert("El resultadod de la operacion es: " + operacion);
 
 // Tipos de datos
+var numero_entero = 44;
+var cadena_texto = 'Hola soy pablo me llaman "el turco"'; // tambien puede ocupar ' '.
+var verdadero_o_falso = true;
+
+var numero_falso = "33";
+
+// convierte numero string a numero de lo contrario solo lo concatena al ser texto
+console.log(Number(numero_falso)+7);
+//console.log(parseInt(numero_falso)+7); // numero redondeado
+//console.log(parseFloat(numero_falso)+7); //numero flotante
+//console.log(String(numero_entero)+4); //numero a texto
