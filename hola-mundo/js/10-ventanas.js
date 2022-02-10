828c1f5ff3e14b0025e50c4c768aea8bfae65766
drPabloIgnacio <gdiaz.pabloignacio@gmail.com>
2022-02-09 23:42:03 -0300

    entrenando funciones js

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+// Funciones
+
+// Alerta
+alert("Esta es una alerta");
+
+// Confirmación
+var mi_resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(mi_resultado);
+
+// INgreso de datos
+var resutlado_edad = prompt("¿Que edad tienes?", 18);
+console.log(resutlado_edad);
